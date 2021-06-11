39fcd90453ed5c9498635d3b0226dc0a7592b893
avoajaugochukwu <karlthegoo@hotmail.com>
2021-06-11 23:40:23 +0100

    Switched to BrowserRouter from HashRouter and gh-pages module

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react'
 // import { BrowserRouter, useLocation } from 'react-router-dom'
-import { HashRouter, useLocation } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 
 import SideBar from './containers/SideBar/SideBar'
 import MainSection from './containers/MainSection/MainSection'
@@ -48,14 +48,14 @@ function App() {
 
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter>
       <ScrollToTop />
         <div className="flex relative">
           <SideBar />
           <MainSection handlePause={handlePause} handlePlay={handlePlay}  />
           <FooterPlayer handlePause={handlePause} handlePlay={handlePlay} state={{ audio: [audio, setAudio]}} />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
