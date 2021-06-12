8303a5a0096a76ea3f2f29a1b410fdaeb6fedcca
avoajaugochukwu <karlthegoo@hotmail.com>
2021-06-12 20:42:53 +0100

    Updated font-size of links

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -33,14 +33,14 @@ const MobileHeader = () => {
           </div>
         </div>
         <div className="mt-3 flex flex-col">
-          <a href="https://github.com/avoajaugochukwu/podcast_player" className="mx-2 text-gray-600 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit">
+          <a href="https://github.com/avoajaugochukwu/podcast_player" className="mx-2 text-sm text-gray-600 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit">
             Github
-                </a>
+          </a>
 
-          <a href="https://twitter.com/avoajacharles" className="mx-2 text-gray-600 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-300"
+          <a href="https://twitter.com/avoajacharles" className="mx-2 text-sm text-gray-600 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-300"
             aria-label="Facebook">
             Twitter
-                </a>
+          </a>
         </div>
       </div>
     </div>
